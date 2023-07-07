c1728f2c100e31bffcff296f0fa23d5bb1839678
WataruShirako <shirako.co.shirako@gmail.com>
2023-07-07 18:17:28 +0900

    svg変更

--- src/components/entt/Entry.tsx
+++ src/components/entt/Entry.tsx
@@ -3,15 +3,18 @@ import Image from 'next/image';
 const Entry = () => {
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 100">
-        <path
-          id="パス_285"
-          data-name="パス 285"
-          d="M0,120H1366V60s-154.1-40-324.849-40S683,60,683,60s-208.446,40-379.2,40S0,60,0,60Z"
-          transform="translate(0 -20)"
-          fill="#252525"
-        />
-      </svg>
+      <div className="svg__container">
+        <svg xmlns="http://www.w3.org/2000/svg" className={'svg__content'} viewBox="0 0 1366 100">
+          <path
+            id="パス_285"
+            data-name="パス 285"
+            d="M0,120H1366V60s-154.1-40-324.849-40S683,60,683,60s-208.446,40-379.2,40S0,60,0,60Z"
+            transform="translate(0 -20)"
+            fill="#252525"
+          />
+        </svg>
+      </div>
+
       <section id="entry" className={'bg-[#252525]'}>
         <h2 className="secTitle">エントリー受付中</h2>
         <div className="ctaBtnArea flex flex-col gap-8 md:flex-row items-end max-w-4xl m-auto">
@@ -51,14 +54,16 @@ const Entry = () => {
           </a>
         </div>
       </section>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 100">
-        <path
-          id="パス_286"
-          data-name="パス 286"
-          d="M1366,0H0V60s154.1,40,324.849,40S683,60,683,60s208.446-40,379.2-40S1366,60,1366,60Z"
-          fill="#252525"
-        />
-      </svg>
+      <div className="svg__container">
+        <svg xmlns="http://www.w3.org/2000/svg" className={'svg__content'} viewBox="0 0 1366 100">
+          <path
+            id="パス_286"
+            data-name="パス 286"
+            d="M1366,0H0V60s154.1,40,324.849,40S683,60,683,60s208.446-40,379.2-40S1366,60,1366,60Z"
+            fill="#252525"
+          />
+        </svg>
+      </div>
     </>
   );
 };
