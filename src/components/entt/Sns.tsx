0ecfe9765f35fbcf74d1c99ac8ac01aab166790c
WataruShirako <shirako.co.shirako@gmail.com>
2023-07-07 19:06:28 +0900

    sec commit

--- src/components/entt/Sns.tsx
+++ src/components/entt/Sns.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image';
 const Sns = () => {
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 100">
-        <path
-          id="パス_285"
-          data-name="パス 285"
-          d="M0,120H1366V60s-154.1-40-324.849-40S683,60,683,60s-208.446,40-379.2,40S0,60,0,60Z"
-          transform="translate(0 -20)"
-          fill="#CBD6D0"
-        />
-      </svg>
+      <div className={'svg__container'}>
+        <svg xmlns="http://www.w3.org/2000/svg" className={'svg__content'} viewBox="0 0 1366 100">
+          <path
+            id="パス_285"
+            data-name="パス 285"
+            d="M0,120H1366V60s-154.1-40-324.849-40S683,60,683,60s-208.446,40-379.2,40S0,60,0,60Z"
+            transform="translate(0 -20)"
+            fill="#CBD6D0"
+          />
+        </svg>
+      </div>
       <section id="ambassador" className={'bg-[#CBD6D0]'}>
         <h2 className="secTitle">
           夢叶えるプロジェクト
@@ -60,14 +62,16 @@ const Sns = () => {
           </div>
         </div>
       </section>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 100">
-        <path
-          id="パス_286"
-          data-name="パス 286"
-          d="M1366,0H0V60s154.1,40,324.849,40S683,60,683,60s208.446-40,379.2-40S1366,60,1366,60Z"
-          fill="#CBD6D0"
-        />
-      </svg>
+      <div className={'svg__container'}>
+        <svg xmlns="http://www.w3.org/2000/svg" className={'svg__content'} viewBox="0 0 1366 100">
+          <path
+            id="パス_286"
+            data-name="パス 286"
+            d="M1366,0H0V60s154.1,40,324.849,40S683,60,683,60s208.446-40,379.2-40S1366,60,1366,60Z"
+            fill="#CBD6D0"
+          />
+        </svg>
+      </div>
     </>
   );
 };
