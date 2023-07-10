f8de46a2d0f55696f3c34642b86de461009f5c4a
WataruShirako <shirako.co.shirako@gmail.com>
2023-07-10 13:41:00 +0900

    commit

--- src/components/entt/Instagram.tsx
+++ src/components/entt/Instagram.tsx
@@ -5,39 +5,39 @@ import { unstable_serialize as infinite_unstable_serialize } from 'swr/infinite'
 
 import useSWR, { Fetcher } from 'swr';
 
-const fetcher = (url: any) => fetch(url).then((res) => res.json());
+// const fetcher = (url: any) => fetch(url).then((res) => res.json());
 
-function Instagram() {
-  const token =
-    'EAAZAKaoh2nDEBAKXgw2XD3paLC8fZAIWnqZCsJaYjry2w3M7rbs13Et87RX4ZB8JmkyDSwO1KTo3SmK3uuwiUAs8BnXugaIVoNhbVUpEiEaPhi64tGWNtsXhNuwXe1aebhLtH0jZBRFC2GGbbDFZCvZACGOIalycxnpBzcZAhVFPmtObVcWciTMB';
-  const { data, error } = useSWR(
-    // `https://graph.facebook.com/v17.0/101854819647114/media?access_token=${token}`,
-    fetcher
-  );
+const Instagram = () => {
+  // const token =
+  //   'EAAZAKaoh2nDEBAKXgw2XD3paLC8fZAIWnqZCsJaYjry2w3M7rbs13Et87RX4ZB8JmkyDSwO1KTo3SmK3uuwiUAs8BnXugaIVoNhbVUpEiEaPhi64tGWNtsXhNuwXe1aebhLtH0jZBRFC2GGbbDFZCvZACGOIalycxnpBzcZAhVFPmtObVcWciTMB';
+  // const { data, error } = useSWR(
+  //   // `https://graph.facebook.com/v17.0/101854819647114/media?access_token=${token}`,
+  //   fetcher
+  // );
 
-  if (error)
-    return (
-      <section id="instagram">
-        <h2 className={`secTitle font-font-cinzel`}>
-          <span className={`block`}>post</span>
-          #夢フォトオーディション
-        </h2>
-        <p className={'text-center'}>データが読み込めませんでした</p>
-      </section>
-    );
+  // if (error)
+  //   return (
+  //     <section id="instagram">
+  //       <h2 className={`secTitle font-font-cinzel`}>
+  //         <span className={`block`}>post</span>
+  //         #夢フォトオーディション
+  //       </h2>
+  //       <p className={'text-center'}>データが読み込めませんでした</p>
+  //     </section>
+  //   );
 
-  if (!data)
-    return (
-      <section id="instagram">
-        <h2 className={`secTitle font-font-cinzel`}>
-          <span className={`block`}>post</span>
-          #夢フォトオーディション
-        </h2>
-        <p className={'text-center'}>ロード中...</p>
-      </section>
-    );
+  // if (!data)
+  //   return (
+  //     <section id="instagram">
+  //       <h2 className={`secTitle font-font-cinzel`}>
+  //         <span className={`block`}>post</span>
+  //         #夢フォトオーディション
+  //       </h2>
+  //       <p className={'text-center'}>ロード中...</p>
+  //     </section>
+  //   );
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <section id="instagram">
@@ -53,6 +53,6 @@ function Instagram() {
       ))} */}
     </section>
   );
-}
+};
 
 export default Instagram;
