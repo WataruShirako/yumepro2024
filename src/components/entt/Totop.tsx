168dee7aa0da3cbda5da15ee9acaed730f9d01b7
WataruShirako <shirako.co.shirako@gmail.com>
2023-07-13 02:12:08 +0900

    toTop追加

--- src/components/entt/Totop.tsx
+++ src/components/entt/Totop.tsx
@@ -1,16 +1,27 @@
+'use client';
+
 import Image from 'next/image';
+import { useRef } from 'react';
 
 const ToTop = () => {
+  const toTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <>
-      <div
+      <button
         id="toTop"
         className={
-          'bg-[#252525] flex justify-center items-center text-[#efefef] h-16 cursor-pointer'
+          'bg-[#252525] flex justify-center items-center text-[#efefef] h-16 cursor-pointer w-full hover:opacity-50'
         }
+        onClick={toTop}
       >
         <p>ページトップへ</p>
-      </div>
+      </button>
     </>
   );
 };
