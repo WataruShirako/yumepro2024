972842f8302bcffc51bd5c2162c49a3ea4e6af9f
WataruShirako <shirako.co.shirako@gmail.com>
2023-07-23 18:53:25 +0900

    リダイレクト

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/project/entertainment/photo',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/project/entertainment/photo',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
