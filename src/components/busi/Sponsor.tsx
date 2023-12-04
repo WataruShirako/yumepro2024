53ef7db2c633b6c9df106b55b2141142855a9e76
yamamoto <xxx.minnnt@gmail.com>
2023-12-04 14:29:46 +0900

    ビジネススポンサー追加

--- src/components/busi/Sponsor.tsx
+++ src/components/busi/Sponsor.tsx
@@ -18,15 +18,12 @@ const Sponsor = () => {
           スポンサー企業さま
         </p>
       </h2>
-      <p className="text-center md:text-5xl text-3xl font-black bg-clip-text bg-gradient-to-tr from-[#C52CFD] to-[#2D4BB5] text-transparent py-20">
-        — COMING SOON —
-      </p>
-      {/*      <div className="container grid grid-cols-3 md:grid-cols-5 gap-3 md:gap-5 px-5 mt-10 mx-auto md:max-w-4xl max-w-lg pb-10">
-        <div className="item">
+      <div className="container grid grid-cols-3 gap-3 md:gap-5 px-5 mt-10 mx-auto md:max-w-4xl max-w-lg pb-10">
+        <div className="item mx-auto">
           <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor01.jpg"}
-            alt={"TKP"}
+            className={"rounded-md "}
+            src={"/top/sponsor/oneli_navi.webp"}
+            alt={"oneli_navi"}
             width={"150"}
             height={"150"}
           />
@@ -34,8 +31,8 @@ const Sponsor = () => {
         <div className="item">
           <Image
             className={"rounded-md mx-auto"}
-            src={"/busi/sponsor02.jpg"}
-            alt={"wiz"}
+            src={"/top/sponsor/tecotec.webp"}
+            alt={"tecotec"}
             width={"150"}
             height={"150"}
           />
@@ -43,77 +40,14 @@ const Sponsor = () => {
         <div className="item">
           <Image
             className={"rounded-md mx-auto"}
-            src={"/busi/sponsor03.jpg"}
-            alt={"CRED"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor04.jpg"}
-            alt={"KIRIN"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor05.jpg"}
-            alt={"DYM"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor06.jpg"}
-            alt={"TECOTEC"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor07.jpg"}
-            alt={"17LIVE"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor08.jpg"}
-            alt={"ミクチャ"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor09.jpg"}
-            alt={"USTUS"}
-            width={"150"}
-            height={"150"}
-          />
-        </div>
-        <div className="item">
-          <Image
-            className={"rounded-md mx-auto"}
-            src={"/busi/sponsor10.jpg"}
-            alt={"USEN"}
+            src={"/top/sponsor/asteeda.png"}
+            alt={"asteeda"}
             width={"150"}
             height={"150"}
           />
         </div>
       </div>
-  */}
+
       <div className="pb-20">
         <a href="" className="button md:text-xl text-base font-bold relative">
           スポンサーさま募集
