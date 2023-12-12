f14639b5c06a4e43eceb5bed066058fe3d1e545b
yamamoto <xxx.minnnt@gmail.com>
2023-12-12 17:46:10 +0900

    2023年修正

--- src/components/top/Link2023.tsx
+++ src/components/top/Link2023.tsx
@@ -1,27 +1,31 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Link2023 = () => {
   return (
     <a
-      href="https://2023.yume-pj.com"
-      className={'block relative mt-12 md:mt-24 hover:brightness-90'}
+      href="http://www.2023.yume-pj.com/"
+      className={"block relative mt-12 md:mt-24 hover:brightness-90"}
     >
       <div
         className={
-          'max-w-[92%] lg:max-w-6xl m-auto rounded-2xl overflow-hidden relative py-5 h-56 md:h-auto'
+          "max-w-[92%] lg:max-w-6xl m-auto rounded-2xl overflow-hidden relative py-5 h-56 md:h-auto"
         }
       >
         <Image
-          src={'/top/2023_link.webp'}
-          alt={''}
-          className={'relative object-cover w-full h-full rounded-2xl overflow-hidden'}
+          src={"/top/2023_link.webp"}
+          alt={""}
+          className={
+            "relative object-cover w-full h-full rounded-2xl overflow-hidden"
+          }
           width={1100}
           height={200}
         />
         <Image
-          src={'/top/2023_link_text.svg'}
-          alt={'夢叶えるプロジェクト2023はこちら'}
-          className={'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 md:w-2/5'}
+          src={"/top/2023_link_text.svg"}
+          alt={"夢叶えるプロジェクト2023はこちら"}
+          className={
+            "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 md:w-2/5"
+          }
           width={330}
           height={75}
         />
