29b46fa3621ea0e7c676845f04218811624c0602
yamamoto <xxx.minnnt@gmail.com>
2024-04-21 03:37:30 +0900

    タイムテーブルデザイン変更

--- src/components/busi/Timetable.tsx
+++ src/components/busi/Timetable.tsx
@@ -19,28 +19,263 @@ const Timetable = () => {
             タイムテーブル
           </p>
         </h2>
-        <p className="container text-left mx-auto px-5 md:px-0 md:max-w-lg pb-20 text-[15px] leading-[1.3rem]">
-          10：00～10：20　　開会式・和田衆議院議員による挨拶 <br />
-          10：20～11：05　　パネルディスカッション① <br />
-          11：05～11：10　　休憩・インターバル <br />
-          11：10～11：55　　パネルディスカッション② <br />
-          11：55～12：00　　休憩・インターバル <br />
-          12：00～12：45　　パネルディスカッション③ <br />
-          12：45～12：50　　休憩・インターバル <br />
-          12：50～13：35　　パネルディスカッション④ <br />
-          13：35～13：40　　休憩・インターバル <br />
-          13：40～14：25　　パネルディスカッション⑤ <br />
-          14：25～14：30　　休憩・インターバル <br />
-          14：30～14：45　　全国学生ピッチ甲子園開会式 <br />
-          14：45～16：05　　学生ピッチ甲子園
-          <br />
-          16：05～16：20　　審査員講評・総評 <br />
-          16：20～16：30　　審査結果発表・表彰 <br />
-          16：30～16：40　　記念撮影（各部門毎及び全体、計4カット） <br />
-          16：40～16：50　　丸尾さん総評 <br />
-          16：50～17：00　　閉会 <br />
-          <br />
-        </p>
+        <div className="font-bold max-w-2xl mx-auto ">
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">10:00 - 10:30</div>
+            <div className="p-2">開会式</div>
+          </div>
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">10:30 - 11:15</div>
+            <div className="p-2">
+              パネルディスカッション
+              <br className="md:hidden" />
+              「音楽×ビジネス」
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">10:30 - 10:45</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">音楽×ビジネス①</div>
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">10:45 - 11:00</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">音楽×ビジネス②</div>
+            </div>
+          </div>
+          <div className="px-2 pb-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">11:00 - 11:15</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">音楽×ビジネス③</div>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">11:30 - 12:15</div>
+
+            <div className="p-2">
+              パネルディスカッション
+              <br className="md:hidden" />
+              「インフルエンサー×ビジネス」
+            </div>
+          </div>
+
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">11:30 - 11:45</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">インフルエンサー×ビジネス①</div>
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">11:45 - 12:00</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">インフルエンサー×ビジネス②</div>
+            </div>
+          </div>
+          <div className="px-2 pb-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">12:00 - 12:15</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">インフルエンサー×ビジネス③</div>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">12:30 - 13:15</div>
+
+            <div className="p-2">
+              パネルディスカッション
+              <br className="md:hidden" />
+              「エンタメ×ビジネス」
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">12:30 - 12:45</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">エンタメ×ビジネス①</div>
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">12:45 - 13:00</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">エンタメ×ビジネス②</div>
+            </div>
+          </div>
+          <div className="px-2 pb-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">13:00 - 13:15</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">エンタメ×ビジネス③</div>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">13:30 - 14:15</div>
+
+            <div className="p-2">
+              パネルディスカッション
+              <br className="md:hidden" />
+              「ファッション×ビジネス」
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">13:30 - 13:45</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">ファッション×ビジネス①</div>
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">13:45 - 14:00</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">ファッション×ビジネス②</div>
+            </div>
+          </div>
+          <div className="px-2 pb-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">14:00 - 14:15</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">ファッション×ビジネス③</div>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">14:30 - 15:15</div>
+
+            <div className="p-2">
+              パネルディスカッション
+              <br className="md:hidden" />
+              「若手IPOについて」
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">14:30 - 14:45</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">若手IPOについて①</div>
+            </div>
+          </div>
+          <div className="px-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">14:45 - 15:00</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">若手IPOについて②</div>
+            </div>
+          </div>
+          <div className="px-2 pb-2 ml-5 sm:ml-12">
+            <div className="bg-gray-100 text-black p-2">15:00 - 15:15</div>
+            <div className="flex">
+              <Image
+                src={"/busi/comingsoon.webp"}
+                width={100}
+                height={100}
+                alt=""
+                className=""
+              />
+              <div className="p-2">若手IPOについて③</div>
+            </div>
+          </div>
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">15:30 - 16:45</div>
+
+            <div className="p-2">ピッチコンテスト</div>
+          </div>
+
+          <div className="p-2">
+            <div className="bg-gray-100 text-black p-2">16:45 - 17:00</div>
+
+            <div className="p-2">授賞式＆閉会式</div>
+          </div>
+        </div>
       </section>
     </>
   );
